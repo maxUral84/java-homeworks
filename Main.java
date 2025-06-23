09b4e3c2a7e1e3c310880a5cb5fbf172af67b9a2
Maks Mavritskiy <max.mavritskiy@gmail.com>
2025-06-23 19:06:56 +0500

    Домашнее задание по теме «Хеш-таблицы»

--- Main.java
+++ Main.java
@@ -1,47 +1,34 @@
-import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
+    public static void main(String[] args) {
+        String source = "CACABABABCCCAABAC";
 
-    public static void main(String[] args) throws IOException {
-        // Заполнение дерева
-        // Названия переменных указывают на место заполняемого узла
-        // например, rl - повернуть на право, затем налево
-        // После заполнения дерево выводится на консоль, можете ориентироваться на него
-
-        Tree ll = new Tree("Александа");
-        Tree lr = new Tree("Владимир");
-        Tree l = new Tree("Борис");
-        l.setLeft(ll);
-        l.setRight(lr);
-
-        Tree rl = new Tree("Иннокентий");
-        Tree rr = new Tree("Пантелеймон");
-        Tree r = new Tree("Константин");
-        r.setLeft(rl);
-        r.setRight(rr);
-
-        Tree root = new Tree("Зоя");
-        root.setLeft(l);
-        root.setRight(r);
-
-        System.out.println(root); // Выведем дерево в консоль
-
-        System.out.println("Проверка поиска по дереву:");
-        System.out.println(root.contains("Иннокентий")); // true
-        System.out.println(root.contains("Борис")); // true
-        System.out.println(root.contains("Анна")); // false
-
-        /* Ваше задание (нужно раскомментировать) */
-        System.out.println("Проверка на пирамидальность по длине имени");
-
-        System.out.println(root.isNamePyramid()); // true
-
-        // Меняем имя в одном из узлов на Павел
-        // Пирамидальность должна нарушиться
-        // А из-за того что имя на ту же букву,
-        // в данном случае свойства дерева поиска сохрнаяются
-        rr.setName("Павел");
-        System.out.println(root.isNamePyramid()); // false
+        System.out.println(hasRepeats(source, 4)); // true
+        System.out.println(hasRepeats(source, 5)); // false
     }
 
+    public static boolean hasRepeats(String source, int size) {
+        Set<LazyString> slices = new HashSet<>();
+        LazyString prev = null;
+
+        for (int i = 0; i <= source.length() - size; i++) {
+            LazyString slice;
+            if (prev == null) {
+                slice = new LazyString(source, i, i + size);
+            } else {
+                slice = prev.shiftRight();
+            }
+
+            if (slices.contains(slice)) {
+                return true;
+            } else {
+                slices.add(slice);
+            }
+            prev = slice;
+        }
+
+        return false;
+    }
 }
