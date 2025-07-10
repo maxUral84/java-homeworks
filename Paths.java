ab23c31cb187ea0695f0b1be2dd845e6589123fe
Maksim Mavritskiy <max.mavritskiy@gmail.com>
2025-07-10 20:13:20 +0500

    Added Task 1: Installation

---
+++ Paths.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+public class Paths {
+    public static final String BASE_DIR = "Games";
+    public static final String SRC_DIR = BASE_DIR + File.separator + "src";
+    public static final String RES_DIR = BASE_DIR + File.separator + "res";
+    public static final String SAVEGAMES_DIR = BASE_DIR + File.separator + "savegames";
+    public static final String TEMP_DIR = BASE_DIR + File.separator + "temp";
+
+    public static final String SRC_MAIN_DIR = SRC_DIR + File.separator + "main";
+    public static final String SRC_TEST_DIR = SRC_DIR + File.separator + "test";
+
+    public static final String MAIN_JAVA = SRC_MAIN_DIR + File.separator + "Main.java";
+    public static final String UTILS_JAVA = SRC_MAIN_DIR + File.separator + "Utils.java";
+
+    public static final String RES_DRAWABLES_DIR = RES_DIR + File.separator + "drawables";
+    public static final String RES_VECTORS_DIR = RES_DIR + File.separator + "vectors";
+    public static final String RES_ICONS_DIR = RES_DIR + File.separator + "icons";
+
+    public static final String TEMP_TXT = TEMP_DIR + File.separator + "temp.txt";
+}
