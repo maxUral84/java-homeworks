5d3ff1079ae175daedc02874f2ce858a11e5d909
Maksim Mavritskiy <max.mavritskiy@gmail.com>
2025-07-20 16:13:07 +0500

    added xml json

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,52 +7,144 @@ import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // CSV to JSON
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String data = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, data);
-        String json = listToJson(list);
+        String csvFileName = "data.csv";
+        List<Employee> csvList = parseCSV(columnMapping, csvFileName);
+        String jsonFromCsv = listToJson(csvList);
+        writeToFile(jsonFromCsv, "data.json");
+
+        // XML to JSON
+        String xmlFileName = "data.xml";
+        List<Employee> xmlList = parseXML(xmlFileName);
+        String jsonFromXml = listToJson(xmlList);
+        writeToFile(jsonFromXml, "data2.json");
+
+        String jsonFile = "data2.json";
+
+        try {
+            // Чтение JSON из файла
+            String json = readString(jsonFile);
+
+            // Преобразование JSON в список сотрудников
+            List<Employee> list = jsonToList(json);
+
+            // Вывод списка сотрудников в консоль
+            for (Employee employee : list) {
+                System.out.println(employee);
+            }
+        } catch (Exception e) {
+            System.err.println("Произошла ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
-    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
-        List<Employee> employees = null;
-        try (Reader reader = new FileReader(fileName)) {
+    // CSV
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (FileReader reader = new FileReader(fileName)) {
             ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
             strategy.setType(Employee.class);
             strategy.setColumnMapping(columnMapping);
 
-            CsvToBean<Employee> csvToBean = new CsvToBeanBuilder<Employee>(reader)
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
                     .withMappingStrategy(strategy)
                     .build();
 
-            employees = csvToBean.parse();
-
+            return csv.parse();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Failed to parse CSV file: " + fileName, e);
         }
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(list);
+    }
+
+    private static void writeToFile(String content, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(content);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write to file: " + fileName, e);
+        }
+    }
+
+    // XML
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> employees = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder xmlContent = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                xmlContent.append(line.trim());
+            }
+
+            String xml = xmlContent.toString();
+            int pos = 0;
+
+            while ((pos = xml.indexOf("<employee>", pos)) != -1) {
+                int endPos = xml.indexOf("</employee>", pos);
+                if (endPos == -1) break;
+
+                String employeeXml = xml.substring(pos + "<employee>".length(), endPos);
+                pos = endPos + "</employee>".length();
+
+                long id = Long.parseLong(getTagValue(employeeXml, "id"));
+                String firstName = getTagValue(employeeXml, "firstName");
+                String lastName = getTagValue(employeeXml, "lastName");
+                String country = getTagValue(employeeXml, "country");
+                int age = Integer.parseInt(getTagValue(employeeXml, "age"));
+
+                employees.add(new Employee(id, firstName, lastName, country, age));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse XML file: " + fileName, e);
+        }
+
         return employees;
     }
 
-    public static String listToJson(List<Employee> list) {
-        Type listType = new TypeToken<List<Employee>>() {}.getType();
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.setPrettyPrinting().create();
-        return gson.toJson(list, listType);
+    private static String getTagValue(String xml, String tag) {
+        String openTag = "<" + tag + ">";
+        String closeTag = "</" + tag + ">";
+
+        int start = xml.indexOf(openTag);
+        if (start == -1) return "";
+
+        start += openTag.length();
+        int end = xml.indexOf(closeTag, start);
+
+        if (end == -1) return "";
+
+        return xml.substring(start, end).trim();
     }
 
-    public static void writeString(String json, String fileName) {
-        try (FileWriter fileWriter = new FileWriter(fileName)) {
-            fileWriter.write(json);
+    // JSON
+    private static String readString(String fileName) {
+        StringBuilder json = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                json.append(line);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
         }
+        return json.toString();
+    }
+
+    private static List<Employee> jsonToList(String json) {
+        Gson gson = new GsonBuilder().create();
+        Type employeeListType = new TypeToken<ArrayList<Employee>>(){}.getType();
+        return gson.fromJson(json, employeeListType);
     }
 }
